9a97e0a6bc460fca7136ae243abfd012261ccea9
Aep <86505664+Aeplet@users.noreply.github.com>
2026-09-07 08:46:30 -0400

    Actually put the new message

--- cogs/mod.py
+++ cogs/mod.py
@@ -69,7 +69,7 @@ class Mod(commands.Cog):
         if old_message.guild is None or old_message.author == self.bot:
             return
         # discord invite check
-        await self.check_discord_invites_message(message=message)
+        await self.check_discord_invites_message(new_message=message)
 
     # todo: warn cog
     @app_commands.default_permissions(moderate_members=True)
