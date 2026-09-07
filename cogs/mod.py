a0ac713f14d09827eeaf65a939a8e21c2372d9bb
Aep <86505664+Aeplet@users.noreply.github.com>
2026-09-07 13:53:22 -0400

    Fix logic bug 2

--- cogs/mod.py
+++ cogs/mod.py
@@ -46,7 +46,7 @@ class Mod(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
         # for moderational on_message, ignore DMs or messages from other bots installed onto the server
-        if message.guild is None or message.author.id == self.bot.id or message.author.bot and isinstance(message.author, discord.Member):
+        if message.guild is None or message.author.id == self.bot.user.id or message.author.bot and isinstance(message.author, discord.Member):
             return
 
         # discord invite check
@@ -66,7 +66,7 @@ class Mod(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message_edit(self, old_message: discord.Message, new_message: discord.Message):
-        if old_message.guild is None or old_message.author.id == self.bot.id or old_message.author.bot and isinstance(old_message.author, discord.Member):
+        if old_message.guild is None or old_message.author.id == self.bot.user.id or old_message.author.bot and isinstance(old_message.author, discord.Member):
             return
         # discord invite check
         await self.check_discord_invites_message(message=new_message)
