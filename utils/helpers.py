d1c5fd732c9e623ad966a99249fa0c83d2dddef9
Aep <86505664+Aeplet@users.noreply.github.com>
2026-09-07 08:44:38 -0400

    Make sure we are in a guild before trying to delete a message

--- utils/helpers.py
+++ utils/helpers.py
@@ -262,6 +262,8 @@ async def post_message_log(messageLog: MessageLog, color: discord.Color, message
         pass # we should probably do something more... idk how to tell them
 
 async def safe_message_delete(message: discord.Message):
+    if message.guild is None:
+        return # safety
     if not message.channel.permissions_for(message.guild.me).manage_messages:
         return
     try:
